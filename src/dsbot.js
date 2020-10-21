e2c062d55723f746dcc028dcb393c39ab1532a2b
Raul <raulccdp@hotmail.com>
2020-10-20 23:42:21 -0300

    adding the report feature and adjusting how it handles commands base on the discord.js guide

--- src/dsbot.js
+++ src/dsbot.js
@@ -1,47 +1,42 @@
-require('dotenv').config()
-
+const fs = require('fs');
 const Discord = require('discord.js');
+const { prefix, token } = require('../config.json');
+
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const cron = require('node-cron')
+
+for (const file of commandFiles) {
+    const command = require(`../commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
 
 try {
-    client.login(process.env.dsBotToken)
+    client.login(token)
 } catch (error) {
     throw `Cannot login properly: ${error}`
 }
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
+    console.log(`Logged in as ${client.user.tag}!`)
+        // cron.schedule("45 16 * * *", commands.autoReport(Discord, client)); // production
+        // cron.schedule("* * * * *", commands.autoReport(client)); // test
+})
 
-let number = 0
+client.on('message', (msg) => {
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
 
-client.on('message', msg => {
-    if (msg.author.username != client.user.username) {
-        if (msg.content.includes("-hh help")) {
-            msg.channel.send(`${msg.author}, currently my commands are: \n-hh play: if you want to start a game\n -hh number {insert your guess here}": to guess the current number`)
-        }
+    const args = msg.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
 
-        if (msg.content == "-hh play") {
-            if (number == 0) {
-                number = Math.ceil(Math.random() * (10 - 1) + 1)
-                msg.channel.send(`Pick a number between ${1} and ${10}`)
-            } else {
-                msg.channel.send(`There is a game in progress, wait for you turn ${msg.author}`)
-            }
-        }
-
-        if (msg.content.includes("-hh number")) {
-            const attachment = (url) => {
-                return new Discord.MessageAttachment(url);
-            }
-
-            if (msg.content.includes(number)) {
-                msg.channel.send(`Nicely done ${msg.author}`, attachment('https://pbs.twimg.com/profile_images/998263990206676992/NcFtEjym_400x400.jpg'))
-                number = 0
-            } else {
-                msg.channel.send(`Wrong number ${msg.author}, try again!`, attachment('https://i.imgur.com/w3duR07.png'))
-                console.log(number)
-            }
-        }
+    if (!client.commands.has(command)) return;
+    console.log(args)
+    try {
+        client.commands.get(command).execute(client, msg, args);
+    } catch (error) {
+        console.error(error);
+        msg.reply('there was an error trying to execute that command!');
     }
-});
+})
